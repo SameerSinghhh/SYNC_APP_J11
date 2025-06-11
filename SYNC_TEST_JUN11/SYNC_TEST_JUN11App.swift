2bfc860b49631d4331cec8255fa10e9795121927
Sammy Singh <sammyss@umich.edu>
2025-06-11 11:11:21 -0700

    Initial Commit

---
+++ SYNC_TEST_JUN11/SYNC_TEST_JUN11App.swift
@@ -0,0 +1,17 @@
+//
+//  SYNC_TEST_JUN11App.swift
+//  SYNC_TEST_JUN11
+//
+//  Created by Sameer Singh on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SYNC_TEST_JUN11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SYNC_TEST_JUN11Tests/SYNC_TEST_JUN11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SYNC_TEST_JUN11Tests.swift
+//  SYNC_TEST_JUN11Tests
+//
+//  Created by Sameer Singh on 6/11/25.
+//
+
+import Testing
+@testable import SYNC_TEST_JUN11
+
+struct SYNC_TEST_JUN11Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
